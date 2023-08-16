109c6ed74519c8d623890eb1e62ad9b23c8d3334
Daniel Petcov <danielpetcov.work@gmail.com>
2023-08-16 18:09:26 +0300

    updated app

--- clearDesktop.py
+++ clearDesktop.py
@@ -1,3 +1,15 @@
 import os
 
-desktop_path = ""
+desktop = "~/Desktop/path"
+
+desktop_path = os.path.expanduser(desktop)
+
+files_on_desktop = os.listdir(desktop_path)
+
+for file_name in files_on_desktop:
+    file_path = os.path.join(desktop_path, file_name)
+    if os.path.isfile:
+        os.remove(file_path)
+        print(f"Deleted: {file_name}")
+
+print("All files on the desktop have been deleted.")
